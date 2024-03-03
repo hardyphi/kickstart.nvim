f1252694dfdf7f7107e411f96eef55645d3e3140
Phil Hardy <pehardy12@gmail.com>
2024-03-03 17:11:25 +0000

    got stylua working by installing locally

--- init.lua
+++ init.lua
@@ -232,7 +232,7 @@ require('lazy').setup {
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',    opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
@@ -267,7 +267,7 @@ require('lazy').setup {
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
@@ -620,27 +620,24 @@ require('lazy').setup {
         -- is found.
         go = { 'gofmt' },
 
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
 
-        markdown = { { "prettierd", "prettier" } },
-        html = { { "prettierd", "prettier" } },
-        yaml = { { "prettierd", "prettier" } },
+        markdown = { { 'prettierd', 'prettier' } },
+        html = { { 'prettierd', 'prettier' } },
+        yaml = { { 'prettierd', 'prettier' } },
 
-        css = { { "prettierd", "prettier" } },
-        scss = { { "prettierd", "prettier" } },
-        less = { { "prettierd", "prettier" } },
+        css = { { 'prettierd', 'prettier' } },
+        scss = { { 'prettierd', 'prettier' } },
+        less = { { 'prettierd', 'prettier' } },
 
-        json = { { "prettierd", "prettier" } },
-        jsonc = { { "prettierd", "prettier" } },
+        json = { { 'prettierd', 'prettier' } },
+        jsonc = { { 'prettierd', 'prettier' } },
 
-        sh = { "formatters.shfmt" },
-        bash = { "formatters.shfmt" },
+        sh = { 'formatters.shfmt' },
+        bash = { 'formatters.shfmt' },
 
-        java = { "formatters.lsp" },
-
-
-
+        java = { 'formatters.lsp' },
       },
     },
   },
@@ -744,7 +741,7 @@ require('lazy').setup {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
