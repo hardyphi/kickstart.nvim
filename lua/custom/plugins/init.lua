328fd17c4bb84f2bf7949f5661266bc2cb505909
Phil Hardy <pehardy12@gmail.com>
2024-03-03 17:08:43 +0000

    adding first custom plugin, CellularAutomaton.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'eandrju/cellular-automaton.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>mr', '<cmd>CellularAutomaton make_it_rain<CR>', { desc = '[m]ake it [r]ain' })
+      vim.keymap.set('n', '<leader>ml', '<cmd>CellularAutomaton game_of_life<CR>', { desc = '[m]ake game of [l]ife' })
+    end,
+  },
+}
